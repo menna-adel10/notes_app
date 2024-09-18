0998cedaee43dc50bebddba9959211f44ec0ba10
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-09-18 13:29:08 +0300

    add custom button and finished bottom sheet

--- lib/views/widgets/add_notes_button_sheet.dart
+++ lib/views/widgets/add_notes_button_sheet.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
+import 'notes_view_body.dart';
+
 class AddNoteButtonSheet extends StatelessWidget {
   const AddNoteButtonSheet({super.key});
 
@@ -9,18 +11,27 @@ class AddNoteButtonSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(hint: 'Title',),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(hint: 'Content',
-          maxLines: 5,),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(hint: 'Title',),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(hint: 'Content',
+            maxLines: 5,),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
 import 'custom_app_bar.dart';
 import 'notes_list_item.dart';
 
@@ -7,7 +9,7 @@ class NotesViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Padding(
+    return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 24.0),
       child: Column(
         children: [
@@ -16,12 +18,35 @@ class NotesViewBody extends StatelessWidget {
           ),
           CustomAppBar(),
           Expanded(child: NotesListView()),
+
         ],
       ),
     );
   }
 }
 
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        color: AppColors.kPrimaryColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: const Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
